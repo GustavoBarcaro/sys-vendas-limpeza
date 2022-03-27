5bb731de0bf51b5298c838bb4aac4c456d5911b8
Gyubido <gubarcaro@gmail.com>
2022-03-27 15:17:53 -0300

    navbar and logout button

---
+++ src/assets/logoutIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function LogoutIcon(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="29"
+      fill="none"
+      viewBox="0 0 32 29"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M17.667 21v1.667a5 5 0 01-5 5H6a5 5 0 01-5-5V6a5 5 0 015-5h6.667a5 5 0 015 5v1.667M24.333 21L31 14.333 24.333 21zM31 14.333l-6.667-6.666L31 14.333zm0 0H7.667 31z"
+      />
+    </svg>
+  );
+}
+
+export default LogoutIcon;
--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { format } from 'date-fns';
+import Button from '@mui/material/Button';
+import { LogoutIcon } from '../../assets';
 import styles from './navbar.module.scss';
 
 export default function NavBar(): JSX.Element {
+  const onClose = () => {
+    window.opener = null;
+    window.open('', '_self');
+    window.close();
+  };
   return (
     <header className={styles.header}>
       <section className={styles.container}>
         <time className={styles.date}>{format(new Date(), 'dd/MM/yyyy')}</time>
         <h2 className={styles.systemName}>Coisa de Faxina</h2>
         <div className={styles.actions}>
-          <div>button</div>
-          <div>sair</div>
+          <div className={styles.logout}>
+            <Button color="inherit" endIcon={<LogoutIcon />} onClick={onClose}>
+              Sair
+            </Button>
+          </div>
         </div>
       </section>
     </header>
