f44d773a60e32f5903411f32fa5683315370e87f
Gyubido <gubarcaro@gmail.com>
2022-04-03 16:36:52 -0300

    register button

---
+++ src/assets/addIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function AddIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      fill="none"
+      viewBox="0 0 22 22"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M11 11H1M11 1v10V1zm0 10v10-10zm0 0h10-10z"
+      />
+    </svg>
+  );
+}
+
+export default AddIcon;
