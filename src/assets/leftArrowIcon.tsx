3cfffd7b060ab364347c78634de3f4710638c26c
ViniciusFascina <vinicius.fascina@outlook.com>
2022-04-20 20:01:00 -0300

    checkout and go back button functionalities

---
+++ src/assets/leftArrowIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function LeftArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="26"
+      height="20"
+      fill="none"
+      viewBox="0 0 26 20"
+    >
+      <path
+        stroke="#3A3A3A"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M1.75 10h22.5M10.5 18.75L1.75 10l8.75 8.75zM1.75 10l8.75-8.75L1.75 10z"
+      />
+    </svg>
+  );
+}
+
+export default LeftArrowIcon;
